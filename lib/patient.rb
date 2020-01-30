4e64b79a42e0516c7254ade4301aa60f55a95180
Taylor Mills <taylormills24@yahoo.com>
2020-01-30 11:39:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -25,3 +25,20 @@ class Patient
   end
 
 end 
+
+doctor_smith = Doctor.new("John Smith")
+Doctor.new("Martha Jones")
+doctor_smith.appointments # should be empty array
+
+doctor_who = Doctor.new("The Doctor")
+doctor_who.name #=> Should be: "The Doctor"
+
+
+appointment = Appointment.new("Friday, January 32nd", doctor_who)
+doctor_who.add_appointment(appointment) #=> 
+
+martin = Patient.new("Martin Jones")
+martin.add_appointment(appointment)
+      
+doctor_who.add_appointment(appointment)
+doctor_who.patients.include?(martin)
